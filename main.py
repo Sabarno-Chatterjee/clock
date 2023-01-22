db8a3c09a5f863ca2b335f19cae00308e82e6900
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-22 19:42:22 +0530

    generates a (1000X1000) turtle green background, with setworldcoordinates()

---
+++ main.py
@@ -0,0 +1,20 @@
+from turtle import Turtle, Screen
+import datetime
+
+screen = Screen()
+screen.title("Clock")
+screen.setup(1000,1000)
+screen.setworldcoordinates(-1000,-1000,1000,1000)
+screen.tracer(0,0)
+screen.bgcolor("yellow green")
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
